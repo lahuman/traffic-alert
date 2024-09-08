7140d53db3261fb051ab1d4ae8a9717adeab60be
Daniel Lim <lahuman@daum.net>
2024-09-08 16:41:07 +0900

    feat : 지도 표시 처리

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
-const express = require('express');
-const  pgExecute = require('./db');
+const express = require("express");
+const path = require("path");
+
+const pgExecute = require("./db");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// EJS 템플릿 엔진 설정
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "public"));
 
-
+// 기본 path를 /public으로 설정(css, javascript 등의 파일 사용을 위해)
+// app.use(express.static(__dirname + '/public'));
 
 const authMiddleware = (req, res, next) => {
-  const authKey = req.header('authKey');
+  const authKey = req.header("authKey");
   if (authKey === process.env.AUTH_KEY) {
     next(); // Authorized
   } else {
-    res.status(403).send('Forbidden: Invalid authKey');
+    res.status(403).send("Forbidden: Invalid authKey");
   }
 };
 
-app.use(authMiddleware);
+app.get("/map", (req, res) => {
+  res.render("map", { authKey: process.env.AUTH_KEY });
+});
 
-app.get('/traffic-alert', async (req, res) => {
+app.get("/traffic-alert", authMiddleware, async (req, res) => {
   const lat = parseFloat(req.query.lat);
   const lon = parseFloat(req.query.lon);
 
   if (!lat || !lon) {
-    return res.status(400).send('Bad Request: Missing or invalid lat/lon');
+    return res.status(400).send("Bad Request: Missing or invalid lat/lon");
   }
 
   const nearbyEvent = await pgExecute`
@@ -50,20 +58,19 @@ app.get('/traffic-alert', async (req, res) => {
   if (nearbyEvent.length > 0) {
     res.json(nearbyEvent);
   } else {
-    res.status(204).send('No Content: No accidents nearby');
+    res.status(204).send("No Content: No accidents nearby");
   }
 });
 
+app.get("/walk-alert", authMiddleware, async (req, res) => {
+  const lat = parseFloat(req.query.lat);
+  const lon = parseFloat(req.query.lon);
 
-app.get('/walk-alert', async (req, res) => {
-    const lat = parseFloat(req.query.lat);
-    const lon = parseFloat(req.query.lon);
-  
-    if (!lat || !lon) {
-      return res.status(400).send('Bad Request: Missing or invalid lat/lon');
-    }
-  
-    const nearbyEvent = await pgExecute`
+  if (!lat || !lon) {
+    return res.status(400).send("Bad Request: Missing or invalid lat/lon");
+  }
+
+  const nearbyEvent = await pgExecute`
           SELECT    
               ST_DISTANCE(
               geography(ST_SetSRID(ST_Point(LONGITUDE, LATITUDE), 4326)),
@@ -79,15 +86,14 @@ app.get('/walk-alert', async (req, res) => {
               )
           order by distance   
     `;
-  
-    if (nearbyEvent.length > 0) {
-      res.json(nearbyEvent);
-    } else {
-      res.status(204).send('No Content: No accidents nearby');
-    }
-  });
 
-  
+  if (nearbyEvent.length > 0) {
+    res.json(nearbyEvent);
+  } else {
+    res.status(204).send("No Content: No accidents nearby");
+  }
+});
+
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+  console.log(`Example app listening on port ${PORT}`);
+});
